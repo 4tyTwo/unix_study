019f39bf76c9642559feda6170f6b770ddf522e1
4tyTwo <i.toporkov@rbkmoney.com>
2018-12-03 09:28:44 +0300

    Rewrote tasks to comply with sample code, fixed indentation

--- Lab4/lr4p1.c
+++ Lab4/lr4p1.c
@@ -3,22 +3,36 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-
 int main() {
-    int tube[2];
-    pipe(tube);
-    if (fork() == 0) {
-      // this is the child process
-      close(tube[0]); // reading end of the pipe
-      dup2(tube[1], 1); // stdout ---> pipe writing end
-	  execl("./substring.out", "substring.out", NULL);
-    }
-    else {
-      if (fork() == 0) {
-        close(tube[1]); // writing end of the pipe
-        dup2(tube[0], 0);  // stdin ----> pipe reading end
-		execl("./case_swap.out", "case_swap.out", NULL);
+  int fd[2];
+  int status;
+  switch (fork()) {
+    case -1:
+      printf("Error while calling fork #1\n");
+      exit(1);
+    case 0:
+      // child 1
+      pipe(fd); // Create a pipe between parent & child
+      switch (fork()) {
+        case -1:
+          printf("Error while calling fork #2\n");
+          exit(2);
+        case 0:
+          close(0); // close stdin
+          dup(fd[0]);
+          close(fd[0]);
+          close(fd[1]); // must be closed so execl won't be paused
+          execl("./substring.out", "substring", 0);
+          exit(0);
+        default:
+          close(1); // close stdout
+          dup(fd[1]);
+          close(fd[1]);
+          close(fd[0]); // must be closed so execl won't be paused
+          execl("./case_swap.out", "case_swap", 0);
       }
-    }
-  return 0;
+    default:
+      wait(&status); // block until childs are finished
+      exit(0);
+  }
 }
--- Lab4/lr4p2.c
+++ Lab4/lr4p2.c
@@ -7,40 +7,41 @@
 #define W 1
 
 int main() {
-    int p[2], q[2];
-    FILE *fdopen(), *fp;
-    pipe(p);
-    pipe(q);
-    if (fork() == 0) {
-        // child
-        // P stream
-        close(p[W]);
-        close(R);
-        dup(p[R]);
-        close(p[R]);
-        // Q stream
-        close(q[R]);
-        close(W);
-        dup(q[W]);
-        close(q[W]);
-        //
-        execl("./substring.out", "substring.out", NULL);
-        printf("error\n");
-    } 
-    else {
-        if (fork() == 0) {
-            close(p[R]);
-            close(q[W]);
-            fp = fdopen(p[W],"w");
-			char st[100];
-			fgets(st, sizeof(st), stdin);
-            fwrite(st, sizeof(char), sizeof(st) + 2, fp);
-            fclose(fp);
-            close(R);
-            dup(q[R]);
-            close(q[R]);
-			execl("./case_swap.out", "case_swap.out", NULL);
-        }
-    }
-    return 0;
+  int p[2], q[2];
+  FILE *fdopen(), *fp;
+  pipe(p);
+  pipe(q);
+  switch (fork()) {
+    case 0:
+      // child
+      // P stream
+      close(p[W]);
+      close(R);
+      dup(p[R]);
+      close(p[R]);
+      // Q stream
+      close(q[R]);
+      close(W);
+      dup(q[W]);
+      close(q[W]);
+      //
+      execl("./substring.out", "substring", 0);
+      printf("Error calling substring\n");
+    case -1:
+      printf("Error while calling fork\n");
+      exit(1);
+    default:
+      close(p[R]);
+      close(q[W]);
+      fp = fdopen(p[W], "w");
+      char st[100];
+      fgets(st, sizeof(st), stdin);
+      fwrite(st, sizeof(char), sizeof(st) + 2, fp);
+      fclose(fp);
+      close(R);
+      dup(q[R]);
+      close(q[R]);
+      execl("./case_swap.out", "case_swap", 0);
+      exit(0);
+  }
 }
